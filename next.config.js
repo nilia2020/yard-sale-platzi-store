35651fd429255b1a4d557450568e8434ebcb2ffa
jorge niglia <jorge.niglia@gmail.com>
2023-05-01 21:16:09 -0300

    hasta configuración de next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  env: {
+    customKey: 'niliaCarton728',
+  },
+  compress: true,
+  async redirects() {
+    return [
+      {
+        source: '/hola',
+        destination: '/hello',
+        permanent: true,
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
